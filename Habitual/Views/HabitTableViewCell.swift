ef7c1f112ae9dd70ddf291e27791b11ca90f5c95
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-03-01 15:47:31 -0800

    added stack view in cell

---
+++ Habitual/Views/HabitTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HabitTableViewCell.swift
+//  Habitual
+//
+//  Created by Gonzalo Birrueta on 3/1/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+class HabitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
