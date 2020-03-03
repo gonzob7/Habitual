e4d5527592e4abab08afea9f98811000ad495f48
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-03-02 23:04:40 -0800

    implemented delete function

--- Habitual/Views/HabitsTableViewController.swift
+++ Habitual/Views/HabitsTableViewController.swift
@@ -69,6 +69,9 @@ extension HabitsTableViewController {
         title = "Habitual"
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pressAddHabit(_:)))
         navigationItem.rightBarButtonItem = addButton
+        
+        navigationItem.leftBarButtonItem = self.editButtonItem
+
     }
 
     @objc func pressAddHabit(_ sender: UIBarButtonItem) {
@@ -90,5 +93,37 @@ extension HabitsTableViewController {
         habitDetailVC.habit = selectedHabit
         habitDetailVC.habitIndex = indexPath.row
         navigationController?.pushViewController(habitDetailVC, animated: true)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+      switch editingStyle {
+        case .delete:
+          let habitToDelete = persistence.habits[indexPath.row]
+          let habitIndexToDelete = indexPath.row
+
+          let deleteAlert = UIAlertController(habitTitle: habitToDelete.title) {
+            self.persistence.delete(habitIndexToDelete)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+          }
+
+          self.present(deleteAlert, animated: true)
+        default:
+          break
+        }
+    }
+}
+
+extension UIAlertController {
+    convenience init(habitTitle: String, comfirmHandler: @escaping () -> Void) {
+        self.init(title: "Delete Habit", message: "Are you sure you want to delete \(habitTitle)?", preferredStyle: .actionSheet)
+
+        let confirmAction = UIAlertAction(title: "Confirm", style: .destructive) { _ in
+            comfirmHandler()
+        }
+        self.addAction(confirmAction)
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        self.addAction(cancelAction)
     }
 }
