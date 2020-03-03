fea1f90dac4498a8b5030801f57fab88a6654f4b
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-03-02 17:02:21 -0800

    started add habit view

---
+++ Habitual/Views/AddHabitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddHabitViewController.swift
+//  Habitual
+//
+//  Created by Gonzalo Birrueta on 3/1/20.
+//  Copyright © 2020 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+class AddHabitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Habitual/Views/HabitsTableViewController.swift
+++ Habitual/Views/HabitsTableViewController.swift
@@ -71,9 +71,14 @@ extension HabitsTableViewController {
 //        navigationItem.rightBarButtonItem = addButton
     }
 //
-//    @objc func pressAddHabit(_ sender: UIBarButtonItem) {
-//        names.insert("Hello, World!", at: 0)
-//        let topIndexPath = IndexPath(row: 0, section: 0)
-//        tableView.insertRows(at: [topIndexPath], with: .automatic)
-//    }
+    @objc func pressAddHabit(_ sender: UIBarButtonItem) {
+//        habits.insert("Hello, World!", at: 0)
+        let topIndexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [topIndexPath], with: .automatic)
+        
+        let addHabitVC = AddHabitViewController.instantiate()
+        let navigationController = UINavigationController(rootViewController: addHabitVC)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true, completion: nil)
+    }
 }
