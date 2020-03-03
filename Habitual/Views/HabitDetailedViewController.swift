f7ad20320f9c98819dbd57ab43849375e842b2da
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-03-02 19:59:17 -0800

    implemented completion/streaks functionality

--- Habitual/Views/HabitDetailedViewController.swift
+++ Habitual/Views/HabitDetailedViewController.swift
@@ -22,13 +22,41 @@ class HabitDetailedViewController: UIViewController {
     @IBOutlet weak var labelStartingDate: UILabel!
     @IBOutlet weak var buttonAction: UIButton!
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateUI()
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func pressActionButton(_ sender: UIButton) {
+        habit = persistence.markHabitAsCompleted(habitIndex)
+        updateUI()
+    }
+    
+    
+    private func updateUI() {
+          title = habit.title
+          imageViewIcon.image = habit.selectedImage.image
+          labelCurrentStreak.text = "\(habit.currentStreak) days"
+          labelTotalCompletions.text = String(habit.numberOfCompletions)
+          labelBestStreak.text = String(habit.bestStreak)
+          labelStartingDate.text = habit.dateCreated.stringValue
+
+          if habit.completedToday {
+              buttonAction.setTitle("Completed for Today!", for: .normal)
+          } else {
+              buttonAction.setTitle("Mark as Completed", for: .normal)
+          }
+      }
+    
+    
 
 
     /*
